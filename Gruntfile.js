5fd56d0bb3e8529fc8b8b2e7f9d0cbe7a071a89f
codingmonkee1 <steve.lin.858@gmail.com>
2015-04-13 17:29:03 -0700

    adding factored out code examples

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,24 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-	webServer: {
-	    rootFolder: "www",
-	    port: 8080
-	}
+
+  grunt.initConfig({
+	   webServer: {
+	      rootFolder: "www",
+	      port: 8080
+	   }
+  });
+
+  grunt.registerTask("default", "start a web server", function(){
+
+  var
+	 webServer = require("./web-server"),
+   webServerConfig = grunt.config("webServer");
+
+  this.async();
+
+  webServer(webServerConfig, function() {
+      grunt.log.writeln("Web server listening on port " + webServerConfig.port)
     });
 
-    grunt.registerTask("default", "start a web server", function(){
+  });
 
-	var
-	   http = require("http"),
-	   express = require("express"),
-	   app = express(),
-	webServerConfig = grunt.config("webServer");
-
-        this.async();
-
-	app.use(express.static(webServerConfig.rootFolder));
-
-	http.createServer(app).listen(webServerConfig.port, function() {
-	    grunt.log.writeln("web server listening on port: " + webServerConfig.port);
-	});
-    });
 };
